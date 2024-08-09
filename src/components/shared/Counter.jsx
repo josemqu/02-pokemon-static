b5ef9e493a8e956c244e5e51cc2fd6633c00e833
josemqu <mailjmq@gmail.com>
2024-08-08 23:22:01 -0300

    fixed counter

--- src/components/shared/Counter.jsx
+++ src/components/shared/Counter.jsx
@@ -9,13 +9,13 @@ export const Counter = () => {
       <h3 class="text-xl">{counter()}</h3>
       <div>
         <button
-          onClick={() => setCounter((prev) => ++prev)}
+          onClick={() => setCounter((prev) => --prev)}
           class="bg-blue-500 p-2 mr-2 rounded-md m-2 font-bold w-12"
         >
           -1
         </button>
         <button
-          onClick={() => setCounter((prev) => --prev)}
+          onClick={() => setCounter((prev) => ++prev)}
           class="bg-blue-500 p-2 mr-2 rounded-md m-2 font-bold w-12"
         >
           +1
